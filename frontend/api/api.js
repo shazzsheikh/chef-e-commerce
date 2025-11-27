48ecdb6506719b9b2d271ed0753d3927c70aa556
shahanshah <97621075+shazzsheikh@users.noreply.github.com>
2025-11-28 01:18:40 +0530

    backend url update

--- frontend/api/api.js
+++ frontend/api/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 export const API = axios.create({
   //   baseURL: "https://your-backend.onrender.com/api", // 🛠️ yahan apna backend URL daal
-  baseURL: "https://lineally-unenervated-eusebia.ngrok-free.dev/api",
+  // baseURL: "https://lineally-unenervated-eusebia.ngrok-free.dev/api",
+  baseURL: "https://chef-e-commerce-api.onrender.com/api",
   withCredentials: true,
   headers: {
     "ngrok-skip-browser-warning": "true", // ✅ This bypasses ngrok splash
