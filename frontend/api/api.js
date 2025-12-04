ec8ca48948aabace89c4dc77eb7c9d84d9dc7293
shenshah <shahanwazh.2000@gmail.com>
2025-12-04 10:45:32 +0530

    aur api update

--- frontend/api/api.js
+++ frontend/api/api.js
@@ -3,8 +3,8 @@ import axios from "axios";
 export const API = axios.create({
   //   baseURL: "https://your-backend.onrender.com/api", // 🛠️ yahan apna backend URL daal
   // baseURL: "https://lineally-unenervated-eusebia.ngrok-free.dev/api",
-  // baseURL: "https://chef-e-commerce-api.onrender.com/api",
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://chef-e-commerce-api.onrender.com/api",
+  // baseURL: "http://localhost:3000/api",
   withCredentials: true,
   headers: {
     "ngrok-skip-browser-warning": "true", // ✅ This bypasses ngrok splash
